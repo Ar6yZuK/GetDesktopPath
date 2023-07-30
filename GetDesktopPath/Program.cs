cc77b4ee751ca25c8a64e33ae2573b3fbe1fdf6e
Ar6yZuK <danya.shubin228@gmail.com>
2023-07-30 03:28:54 +0300

    Добавьте файлы проекта.

---
+++ GetDesktopPath/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.TreatControlCAsInput = true;
+
+var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+Console.WriteLine(desktopPath);
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("Ctrl + C");
+Console.ResetColor();
+Console.WriteLine(" for copy to clipboard");
+
+var keyInfo = Console.ReadKey(true);
+if(keyInfo.Key is ConsoleKey.C
+&& keyInfo.Modifiers is ConsoleModifiers.Control)
+{
+	var t = new Thread(_ => { Clipboard.SetText(desktopPath); Console.WriteLine("Copied to clipboard!"); });
+	t.SetApartmentState(ApartmentState.STA);
+	t.Start();
+	t.Join();
+}
